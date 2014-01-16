c61901b52c5c4e94189f1edd1a5be27283ee3c5f
Jesper Anneback <jesper.anneback@gmail.com>
2014-01-16 10:13:56 +0100

    renamed euler8 to euler10

---
+++ euler10/euler10.java
@@ -0,0 +1,37 @@
+public class euler8 {
+
+
+	//private int arrayList<Integer> primeList;
+	private long sumOfPrime;
+	private final int CAPACITY = 2000000;
+
+	public euler8 () {
+		this.sumOfPrime = 2;
+		for (int i = 3; i < CAPACITY; i+=2) {
+		//for (int i = CAPACITY; i >= 2; i--) {
+
+			if(isPrime(i)) {
+				sumOfPrime += i;
+			}
+		}
+		System.out.println(sumOfPrime);
+	}
+
+	/*
+	prime determinator!
+	*/
+	public boolean isPrime(int n) {
+		if (n%2==0&&n!=2) return false;
+		for(int i = 3 ; i < n; i+=2) {
+			if(n%i==0){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new euler8();
+	}
+}
