02aad2d2a5d3bc8145e691e100dce3db00204c76
Jesper Anneback <jesper.anneback@gmail.com>
2014-01-29 15:18:32 +0100

    Added Euler9

---
+++ Euler9/Euler9.java
@@ -0,0 +1,25 @@
+public class Euler9 {
+
+	/*
+	* A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+		a^2 + b^2 = c^2
+	
+	For example, 3^2 + 4^2 = 9 + 16 = 5^2 = 52.
+
+	There exists exactly one Pythagorean triplet for which a^2 + b^2 + c^2 = 1000.
+	Find the product abc.
+	*/
+
+	public Euler9 (){
+
+	}
+
+	public 
+
+	public static void main(String[] args) {
+		new Euler9();
+	}
+
+}
+//lol
